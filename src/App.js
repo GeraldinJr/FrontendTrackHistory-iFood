ac76f0acffcd04a49ffedbad07e8f8916fe572f4
lucasfpds <lucasfpd.santos@gmail.com>
2022-03-21 14:14:42 -0300

    feat(front): [BLF-01] creating hml branch

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Tela inicial do projeto.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
