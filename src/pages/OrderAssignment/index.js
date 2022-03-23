29c75a0730b671ff714707084df12826b6999a03
magnoliamedeiros <magnoliamedeiros@gmail.com>
2022-03-23 18:20:45 -0300

    feat(front): [BJF-11] order assignment

--- src/pages/OrderAssignment/index.js
+++ src/pages/OrderAssignment/index.js
@@ -2,6 +2,7 @@ import "./styles.css";
 
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
+import imgLogin from "../../assets/login.png";
 
 export default function OrderAssignment() {
 
@@ -9,10 +10,12 @@ export default function OrderAssignment() {
         <main>
             <div className="assignment">
                 <h2>Pedido #123</h2>
+                <h3>Cliente: #123</h3>
                 <div className="div-btn-assignment">
                     <Stack direction="row" spacing={2}>
                         <Button size="large">Iniciar Tracking</Button>
                     </Stack>
+                    {/* <img src={imgLogin} alt="imagem login" /> */}
                 </div>
             </div>
         </main>
