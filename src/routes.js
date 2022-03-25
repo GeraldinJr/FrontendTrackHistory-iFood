d194e20a6312273536e0a5df77a0d3ab8e983cca
lucasfpds <lucasfpd.santos@gmail.com>
2022-03-25 14:21:46 -0300

    feat(front): [BLF-12] change order

--- src/routes.js
+++ src/routes.js
@@ -13,11 +13,13 @@ import Header from "./components/Header";
 import OrderAssingnment from "./pages/OrderAssignment";
 import TrackingOrder from "./pages/TrackingOrder/TrackingOrder";
 import Modal from "./components/Modal/Modal";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
 
 function ProtectedRoute(props) {
   const { token } = useGlobal();
   return (
-    <Route render={() => (token ? props.children : <Redirect to="/login" />)} />
+    <Route render={() => (token ? props.children : <Redirect to="/" />)} />
   );
 }
 
@@ -28,6 +30,8 @@ export default function Routes(params) {
         <Switch>
           <GlobalProvider>
             <Route exact path="/login" component={SignIn} />
+            <Route exact path="/cadastrar" component={SignUp} />
+            <Route exact path="/" component={Home} />
             <ProtectedRoute>
               <Header texto="lucas" />
               <Route path="/pedidos" exact component={Orders} />
